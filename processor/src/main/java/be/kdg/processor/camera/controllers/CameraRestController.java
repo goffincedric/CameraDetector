ce72a39c6cc3bd958df28a830e2a09110f569967
goffincedric <goffincedric@users.noreply.github.com>
2018-10-11 18:27:33 +0200

    Extensions
    - Read multiple files from directory (file-gen)
    - Adjustable concurrent random message generation
    - Some extras

--- processor/src/main/java/be/kdg/processor/camera/controllers/CameraRestController.java
+++ processor/src/main/java/be/kdg/processor/camera/controllers/CameraRestController.java
@@ -1,23 +1,29 @@
-package be.kdg.processor.controller;
+package be.kdg.processor.camera.controllers;
 
 import be.kdg.processor.camera.Camera;
 import be.kdg.processor.camera.CameraLocation;
 import be.kdg.processor.camera.CameraType;
 import be.kdg.processor.camera.Segment;
 import be.kdg.processor.camera.repository.CameraRepository;
+import be.kdg.processor.camera.services.CameraServiceAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class CustomerRestController {
+@RequestMapping("/api/cameras")
+public class CameraRestController {
+    private final CameraServiceAdapter cameraServiceAdapter;
+
     @Autowired
-    CameraRepository repository;
-       
-    @RequestMapping("/save")
+    public CameraRestController(CameraServiceAdapter cameraServiceAdapter) {
+        this.cameraServiceAdapter = cameraServiceAdapter;
+    }
+
+    @RequestMapping("/init")
     public String process(){
-        repository.save(new Camera(1, new CameraLocation(1D, 1D), new Segment(2, 200, 120), 4, CameraType.SPEED_EMISSION));
+        cameraServiceAdapter.createCamera(new Camera(1, new CameraLocation(1D, 1D), new Segment(2, 200, 120), 4, CameraType.SPEED_EMISSION));
         return "Done";
     }
        
--- processor/src/main/java/be/kdg/processor/camera/controllers/WebController.java
+++ processor/src/main/java/be/kdg/processor/camera/controllers/WebController.java
@@ -1,4 +1,4 @@
-package be.kdg.processor.controller;
+package be.kdg.processor.camera.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
