19b2dd6b0370c7d9ea056f45e10ae66758729afb
goffincedric <goffincedric@users.noreply.github.com>
2018-10-18 23:28:47 +0200

    Web fine settings

--- processor/src/main/java/be/kdg/processor/security/SecurityConfig.java
+++ processor/src/main/java/be/kdg/processor/security/SecurityConfig.java
@@ -1,10 +1,10 @@
 package be.kdg.processor.security;
 
-import org.apache.tomcat.jni.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
@@ -16,7 +16,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/home", "/api/**").permitAll()
+                .antMatchers("/", "/home", "/api/**", "/css/**").permitAll()
                 .antMatchers("/admin", "/h2-console/**").hasRole("ADMIN").anyRequest()
                 .authenticated()
                 .and()
@@ -35,4 +35,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .withUser("admin").password("{noop}admin").roles("ADMIN");
     }
+
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web.ignoring().antMatchers("/resources/**", "/css/**");
+//    }
 }
